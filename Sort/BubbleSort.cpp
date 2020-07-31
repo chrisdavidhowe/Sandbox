c8c453a9e50c80eddcf805248c2974b8ae4cb8d3
Chris Howe <chrisdavidhowe@moogmusic.com>
2020-07-31 17:34:07 -0400

    update slightly optimized

--- Sort/BubbleSort.cpp
+++ Sort/BubbleSort.cpp
@@ -9,16 +9,16 @@ int bubbleSort(int* data, int length)
     int i = 0;
     int j = 0;
 
-    for (i = 0; i < length; ++i)
+    for (i = 0; i < length; i++)
     {
-        for (j = 0; j < length; ++j)
+        for (j = 0; j < (length - i); j++)
         {
-            if (data[i] < data[j])
+            if (data[j] > data[j+1])
             {
-                swap(&data[i], &data[j]);
+                swap(&data[j], &data[j+1]);
             }
 
-            if (data[i] == data[j])
+            if (data[j] == data[j+1])
             {
             }
         }
